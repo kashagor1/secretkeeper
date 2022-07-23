2f5996f69765c0c09cc8b82a47ec6bc5c2eb3317
S M Khirul Alam <ka.shagor1@gmail.com>
2022-07-23 16:17:58 +0600

    Basic User Authentication Implemented

--- app.js
+++ app.js
@@ -1,1 +1,71 @@
-//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://127.0.0.1:27017/secretsDB",{useNewUrlParser:true});
+
+
+
+const userSchema = {
+	email:String,
+	password:String
+}
+
+const user =  new mongoose.model("User",userSchema);
+
+
+
+app.get('/',(req,res)=>{
+	res.render("home");
+});
+
+app.get("/login",(req,res)=>{
+	res.render("login");
+});
+
+app.post("/login",(req,res)=>{
+	let eil = req.body.username;
+	let pa = req.body.password;
+	user.findOne({email:eil},(err,foundUser)=>{
+		if(err){
+			console.log(err);
+		}else{
+			if(foundUser){
+				if(foundUser.password=== pa){
+					res.render("secrets");
+				}
+			}
+		}
+	})
+})
+
+
+app.get("/register",(req,res)=>{
+	res.render("register");
+});
+app.post("/register",(req,res)=>{
+	let eil = req.body.username;
+	let pa    = req.body.password;
+	const newUser = new user({
+		email:eil,
+		password:pa
+	});
+	newUser.save((err)=>{
+		if(err){
+			console.log(err);
+		}else{
+			res.render("secrets");
+		}
+	})
+})
+
+app.listen(3000,()=>{
+	console.log("server started on port 3000");
+})
